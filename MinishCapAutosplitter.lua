b23f879eaef56c4bdc6519279a69840129e34311
Dalton Harenza <djharenz@buffalo.edu>
2019-10-11 11:53:38 -0400

    Update MinishCapAutosplitter.lua

--- MinishCapAutosplitter.lua
+++ MinishCapAutosplitter.lua
@@ -1,10 +1,19 @@
 -- Minish Cap autosplitter for LiveSplit
--- Semirare, 2019 https://github.com/semirare
+-- Semirare, 2019 https://github.com/icemirrors18/Minish-Cap-Autosplitter
 -- Requires LiveSplit 1.7+ See readme for setup information
+
+--CHANGE THE BELOW LINE BASED ON WHAT CATEGORY YOU ARE RUNNING
+--change to one of the following: "ANY", "FIREROD", "GLITCHLESS", "HUNDO"
+--make sure you have your "MinishCapAutosplitterConfig_(category name).txt" file setup for your splits
+category = "ANY"
 
 current_split = 1
 done = false
 started = false
+
+--used for hundo kinstone split
+kinstoneCave = false
+kinstoneField = false
 
 local address_table = 
 {
@@ -57,6 +66,7 @@ local location_table =
 ["WARPHYLIA"] = {11,0},
 ["WINDRUINS"] = {5,0},
 ["CLOUDTOPS"] = {8,1},
+["LEAVECLOUDTOPS"] = {3,1}
 }
 
 --used to store splits based on how the user has set them up - populated in the establish_splits function
@@ -101,6 +111,30 @@ local function compare_locations(goal)
 		current_split = current_split + 1	
 	end
 end
+
+local function check_kinstone_split()
+	--hundo why do you do this
+	local player_loc = get_player_location()
+	if (kinstoneCave) then
+		if (kinstoneField) then
+			--if both cave and field have been entered, split upon town entry
+			if (player_loc[1] = 2 and player_loc[2] = 0) then
+				tcp_connection:send("split\r\n")
+				current_split = current_split + 1
+			else
+		else
+			--if cave has been entered but field hasn't check for field entry
+			if (player_loc[1] == 3 and player_loc[2] == 7) then
+				kinstoneField = true
+			end
+		end
+	else
+		--check for cave entry
+		if (player_loc[1] == 50 and player_loc[2] == 18) then
+			kinstoneCave = true;
+		end
+	end
+	
 
 --checks flag for item/dungeon entry/other flagged event
 local function check_flag(address,bit)
@@ -184,21 +218,7 @@ end
 
 --set up for customized splits
 local function establish_splits()
-	io.write("What category are you running? (A = Any%, F = Firerod, G = Glitchless, H = Hundo):")
-	local answer = io.read()
-	if (answer == "A") then
-		answer = "ANY"
-	elseif (answer == "F") then
-		answer = "FIREROD"
-	elseif (answer = "G") then
-		answer = "GLITCHLESS"
-	elseif (answer = "H") then
-		answer = "HUNDO"
-	else
-		io.write("Invalid category, please enter on of the following(letter only): (A = Any%, F = Firerod, G = Glitchless, H = Hundo)"
-		establish_splits()
-	end
-	local file = io.open("MinishCapAutosplitterConfig" .. answer .. ".txt", 'r')
+	local file = io.open("MinishCapAutosplitterConfig" .. category .. ".txt", 'r')
 	local splitNum = 1
 	local splitInfo = {}
 	for line in file:lines() do
