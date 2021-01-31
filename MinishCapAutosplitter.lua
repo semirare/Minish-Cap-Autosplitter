62911200bd837e2f9d8d0954622a4d5aaf7b4265
Dalton Harenza <dalton.harenza@gmail.com>
2021-01-30 21:31:43 -0700

    Add files via upload

--- MinishCapAutosplitter.lua
+++ MinishCapAutosplitter.lua
@@ -3,71 +3,17 @@
 -- Requires LiveSplit 1.7+ See readme for setup information
 
 --CHANGE THE BELOW LINE BASED ON WHAT CATEGORY YOU ARE RUNNING
---change to one of the following: "ANY", "FIREROD", "GLITCHLESS", "HUNDO"
+--change to one of the following: "ANY", "FIREROD", "GLITCHLESS", "HUNDO", "PK22AH"
 --make sure you have your "MinishCapAutosplitterConfig_(category name).txt" file setup for your splits
-category = "HUNDO"
+category = "ANY"
 
 current_split = 1
 done = false
 started = false
 
---used for hundo kinstone split
+--used for weird hundo splits
 kinstoneCave = false
 kinstoneField = false
-
-local address_table = 
-{
-["EZLO"] = 0x002C9E,
-["ENTERDWS"] = 0x002AA3,
-["GUSTJAR"] = 0x002B36,
-["ENTERCHU"] = 0x002D45,
-["EARTHELEMENT"] = 0x002B42,
-["CRENELPLANT"] = 0x002CC5,
-["GRIPRING"] = 0x002B43,
-["ENTERCOF"] = 0x002AA3,
-["CANE"] = 0x002B36,
-["FIREELEMENT"] = 0x002B42,
-["BOOTS"] = 0x002B37,
-["BOW"] = 0x002B34,
-["ENTERFOW"] = 0x002AA3,
-["MOLEMITTS"] = 0x002B36,
-["OCARINA"] = 0x002B37,
-["FLIPPERS"] = 0x002B43,
-["BOOMERANG"] = 0x002B35,
-["ENTERTOD"] = 0x002AA3,
-["LANTERN"] = 0x002B35,
-["WATERELEMENT"] = 0x002B42,
-["ROYALVALLEY"] = 0x002D02,
-["ENTERPOW"] = 0x002AA3,
-["CAPE"] = 0x002B37,
-["WINDELEMENT"] = 0x002B42,
-["ENTERDHC"] = 0x002AA3,
-["SPAWNNUTS"] = 0x002DC4,
-["DHCKEY"] = 0x002DBC,
-["SIMON"] = 0x002B43,
-["CUCCOS"] = 0x002CA5,
-["BOTTLES"] = 0x002B29,
-["TINGLE"] = 0x002B41,
-["FIGURINES"] = 0x002B41,
-["MIRROR"] = 0x002B35,
-["OCTOSUCKS"] = 0x002D97
-}
-
-local location_table =
-{
-["GLEEROK"] = {81, 0},
-["MAZAAL"] = {88, 22},
-["OCTO"] = {96,14},
-["HOUSEOFWINDS"] = {8,0},
-["GYORG"] = {113,0},
-["DHCBOSSHALL"] = {141,0},
-["DARKNUTS"] = {137,0},
-["V1"] = {140,0},
-["WARPCRENEL"] = {6,2},
-["WARPHYLIA"] = {11,0},
-["CLOUDTOPS"] = {8,1},
-["LEAVECLOUDTOPS"] = {3,1}
-}
 
 --used to store splits based on how the user has set them up - populated in the establish_splits function
 local splits_table ={}
@@ -103,10 +49,9 @@ local function get_player_location()
 end
 
 --checks if the player's location matches the goal location and splits if it does
-local function compare_locations(goal)
+local function compare_locations(areaid, roomid)
 	local player_loc = get_player_location()
-	local goal_loc = location_table[goal]
-	if (player_loc[1] == goal_loc[1] and player_loc[2] == goal_loc[2]) then
+	if (player_loc[1] == areaid and player_loc[2] == roomid) then
 		tcp_connection:send("split\r\n")
 		current_split = current_split + 1	
 	end
@@ -140,7 +85,7 @@ end
 local function check_flag(address,bit)
 	--read address
 	memory.usememorydomain("EWRAM")
-	read_byte = memory.readbyte(address)
+	read_byte = memory.readbyte(tonumber(address))
 	--convert to 8 bit binary
 	read_byte = toBits(read_byte, 8)
 	--check the correct bit in the binary
@@ -151,14 +96,20 @@ local function check_flag(address,bit)
 	end
 end
 
---gets the player's current location to the location of each boss room
-local function check_area_entry(goalName)
+--check if the player has entered  the goal room
+local function check_area_entry(areaid, roomid)
 	if (goalName == "WINDRUINS") then
 		--check for castor minish cave HP then location
+		if check_flag(0x2D23, 8) then
+			compare_locations(5, 0)
+		end			
 	elseif (goalName == "LEAVECLOUDTOPS") then
 		--check for light arrows then location
+		if check_flag(0x2B34, 4) then
+			compare_locations(3, 1)
+		end	
 	else
-		compare_locations(goalName)
+		compare_locations(areaid, roomid)
 	end
 end
 
@@ -196,13 +147,12 @@ end
 local function check_current_split()
 	local split = splits_table[current_split]
 	if (split[1] == "FLAG") then
-		check_flag(address_table[split[2]], tonumber(split[4]))
-	elseif (split[1] == "ENTERAREA") then
-		check_area_entry(split[2])
+		check_flag(split[2], tonumber(split[3]))
+	elseif (split[1] == "LOCATION") then
+		check_area_entry(tonumber(split[2]), tonumber(split[3]))
 	elseif (split[1] == "KINSTONES") then
 		check_kinstone_split()
 	else
-		--vaati splits
 		check_vaati(split[2])
 	end
 end
@@ -227,18 +177,11 @@ local function establish_splits()
 	for line in file:lines() do
 		if (string.sub(line,1,2) ~= "--") then
 			splitInfo = splitString(line, ",")
-			if (splitInfo[1] == "FLAG") then
-				splitInfo[3] = address_table[splitInfo[2]]
-			end
 			splits_table[splitNum] = splitInfo
 			splitNum = splitNum + 1
 		end
 	end
 	print("Using category: " .. category)
-	print("Using splits:")
-	for k, v in pairs(splits_table) do
-		print(k .. ": " .. v[2])
-	end
 end
 
 -- set up the livesplit connection
